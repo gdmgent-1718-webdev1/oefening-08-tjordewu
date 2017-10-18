5ddb4e1dbe75d842fee16fc7d7c7560bad0ebd8a
gdm-1718-tjordewu <tjordewu@student.arteveldehs.beecho>
2017-10-18 22:39:30 +0200

    first commit

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
 
-function checkAge($age){
-    if($age < 16){
-        return $age . " jaar is te jong voor deze film";
-    } 
-    else if($age >= 16 && $age < 18) {
-        return $age . " jaar, je hebt ouderlijk toezicht nodig.";
-    }  
-    else{
-        return "Je bent al $age jaar. Geniet van de film!";
+function fiftyShades () {
+    for ($i = 0; $i < 50; $i++){
+        $kleur = $i;
+        echo "<div style='background-color: rgb({$kleur},{$kleur},{$kleur}); width: 20px; height: 20px; border: 1px solid white;'></div><br>" , PHP_EOL;
     }
 }
+fiftyShades();
 
-for( $i = 1; $i<19; ++$i){
-    echo checkAge($i) . "<br>";
-}
 ?>
